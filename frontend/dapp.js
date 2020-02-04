f12ff04fbc17dda9c31bf35bd913aa147e5e0200
Jason Stewart <jason.stewart@gmail.com>
2020-02-03 20:06:53 -0500

    Updated contract address

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // Change this address to match your deployed contract!
-const contract_address = "0xa5b70b3f9e02b2d0b85E10042a76b1A7F397Cc6b";
+const contract_address = "0x2d3881B4bd9FBACE54d796176255C92956394eb2";
 
 const dApp = {
   ethEnabled: function() {
